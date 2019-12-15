9f506d7492bd3853c591afd19b84737d7cd1bc92
RobnettSean22 <robnettsean22@gmail.com>
2019-12-15 00:14:56 -0700

    temp for server set no endpoints

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const session = require("express-session");
+const app = express();
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+app.use(express.json());
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 14
+    }
+  })
+);
+
+massive(CONNECTION_STRING).then(dbInstance => {
+  console.log("database is lit");
+  app.set("db", dbInstance);
+});
+
+let port = SERVER_PORT || 4001;
+app.listen(port, () => console.log(`catch me outside on ${port}`));
